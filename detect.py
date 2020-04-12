9d9fe4a4a3cece5cf1f13590dbd6b12730ce853b
tienhaophung <tienhaophung@gmail.com>
2020-04-12 21:45:39 +0700

    Add 1 image prediction

--- detect.py
+++ detect.py
@@ -21,13 +21,18 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.ticker import NullLocator
 
+import numpy as np
+import random
+
+import torchvision.transforms as transforms
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
     parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
     parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/coco.names", help="path to class label file")
-    parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
+    parser.add_argument("--conf_thres", type=float, default=0.5, help="object confidence threshold")
     parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
     parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
     parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
@@ -52,41 +57,74 @@ if __name__ == "__main__":
 
     model.eval()  # Set in evaluation mode
 
-    dataloader = DataLoader(
-        ImageFolder(opt.image_folder, img_size=opt.img_size),
-        batch_size=opt.batch_size,
-        shuffle=False,
-        num_workers=opt.n_cpu,
-    )
-
     classes = load_classes(opt.class_path)  # Extracts class labels from file
-
     Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
     imgs = []  # Stores image paths
     img_detections = []  # Stores detections for each image index
 
-    print("\nPerforming object detection:")
-    prev_time = time.time()
-    for batch_i, (img_paths, input_imgs) in enumerate(dataloader):
-        # Configure input
-        input_imgs = Variable(input_imgs.type(Tensor))
+    if ".jpg" in opt.image_folder:
+        img = transforms.ToTensor()(Image.open(opt.image_folder))
+        
+        # Pad to square resolution
+        img, _ = pad_to_square(img, 0)
+        # Resize
+        img = resize(img, opt.img_size).unsqueeze(0)
+
+        print("\nPerforming object detection:")
+        prev_time = time.time()
+        
+        # Add to cuda buffers
+        input_img = Variable(img.type(Tensor))
 
         # Get detections
         with torch.no_grad():
-            detections = model(input_imgs)
-            detections = non_max_suppression(detections, opt.conf_thres, opt.nms_thres)
+            detection = model(input_img)
+            detection = non_max_suppression(detection, opt.conf_thres, opt.nms_thres)
 
         # Log progress
         current_time = time.time()
         inference_time = datetime.timedelta(seconds=current_time - prev_time)
         prev_time = current_time
-        print("\t+ Batch %d, Inference Time: %s" % (batch_i, inference_time))
+        print("\t+ Inference Time: %s" % (inference_time))
 
         # Save image and detections
-        imgs.extend(img_paths)
-        img_detections.extend(detections)
+        imgs.extend([opt.image_folder,])
+        img_detections.extend(detection)
 
+    else:
+
+        dataloader = DataLoader(
+            ImageFolder(opt.image_folder, img_size=opt.img_size),
+            batch_size=opt.batch_size,
+            shuffle=False,
+            num_workers=opt.n_cpu,
+        )
+
+
+
+        print("\nPerforming object detection:")
+        prev_time = time.time()
+        for batch_i, (img_paths, input_imgs) in enumerate(dataloader):
+            # Configure input
+            input_imgs = Variable(input_imgs.type(Tensor))
+
+            # Get detections
+            with torch.no_grad():
+                detections = model(input_imgs)
+                detections = non_max_suppression(detections, opt.conf_thres, opt.nms_thres)
+
+            # Log progress
+            current_time = time.time()
+            inference_time = datetime.timedelta(seconds=current_time - prev_time)
+            prev_time = current_time
+            print("\t+ Batch %d, Inference Time: %s" % (batch_i, inference_time))
+
+            # Save image and detections
+            imgs.extend(img_paths)
+            img_detections.extend(detections)
+
+    # ====== VISUALIZE and SAVE IMAGES ======
     # Bounding-box colors
     cmap = plt.get_cmap("tab20b")
     colors = [cmap(i) for i in np.linspace(0, 1, 20)]
@@ -106,6 +144,7 @@ if __name__ == "__main__":
         # Draw bounding boxes and labels of detections
         if detections is not None:
             # Rescale boxes to original image
+            # print(detections)
             detections = rescale_boxes(detections, opt.img_size, img.shape[:2])
             unique_labels = detections[:, -1].cpu().unique()
             n_cls_preds = len(unique_labels)
@@ -136,6 +175,6 @@ if __name__ == "__main__":
         plt.axis("off")
         plt.gca().xaxis.set_major_locator(NullLocator())
         plt.gca().yaxis.set_major_locator(NullLocator())
-        filename = path.split("/")[-1].split(".")[0]
+        filename = path.split(os.sep)[-1].split(".")[0]
         plt.savefig(f"output/{filename}.png", bbox_inches="tight", pad_inches=0.0)
         plt.close()
